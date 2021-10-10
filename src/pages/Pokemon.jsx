4170e4b8c3645ec28db5bb3f69385539928c9624
alexandercode1993 <alexandersala2015@gmail.com>
2021-10-10 15:05:57 -0300

    init

--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import MainHeader from "../Component/MainHeader";
+import PokemonService from '../Services/PokemonService';
+
+
 
 const Pokemon = ()=> {
+
+    const [pokemon, setPokemon] = useState({});
+    const [loading,setLoading] = useState(true);
+    const [error,setError] = useState(null);
+    const [intervalId,setIntervalID]=useState();
+    const [base,setBase]= useState('se');
+
+    const FetchData = async () =>{
+
+        setLoading(true);
+        setError(null);
+        try{
+            const pokemon = await PokemonService.all();
+
+            setPokemon(pokemon);
+            setLoading(false);
+        }catch{
+            setLoading(false);
+            setError(error);
+        }
+       
+        useEffect(setBase('hola'));
+        console.log(base);
+        const nueva = 'hola nuevo';
+        setBase(nueva);
+        console.log(base)
+    }
     return(
         <>
-        <h1>pokimon</h1>
+        <h1>{base}</h1>
+        <MainHeader/>
+        
         </>
     );
 }
